04adefeed23338f039242d96ac60435e181460a4
apw601 <apw601@gmail.com>
2018-06-14 22:51:16 +0000

    Done.

--- personality_quiz.rb
+++ personality_quiz.rb
@@ -1,90 +1,97 @@
 require 'colorize'
 
-question_1 = 0
-question_2 = 0 
-question_3 = 0 
-question_4 = 0 
-question_5 = 0 
-question_6 = 0 
-question_7 = 0 
-question_8 = 0 
-question_9 = 0 
-question_10 = 0
-total = question_1 + question_2 + question_3 + question_4 + question_5 + question_6 + question_7 + question_8 + question_9 + question_10
-
 puts "what's your mood? quiz".upcase
 sleep(1.5)
 def mood
+  happy = 0
+  stressed = 0 
+  peaceful = 0 
+  upset = 0
 puts "do you believe in yourself?".upcase
   value_1 = gets.chomp.to_i
 if value_1 == "yes" 
-
+  happy += 1
+  peaceful += 1
 else value_1 == "no"
-
+  upset += 1 
+  stressed += 1
 end
 puts "are you currently satisfied with your love life? ".upcase
   value_2= gets.chomp.to_i
 if value_2 == "yes"
-
+  happy += 1
+  peaceful += 1
 else value_2 == "no"
-
+  upset += 1 
+  stressed += 1
 end
 puts "could an animal cheer you up right now?".upcase
   value_3 = gets.chomp.to_i
 if value_3 == "yes"
-
+  upset += 1 
+  stressed += 1
 else value_3 == "no"
-  
+  happy += 1
+  peaceful += 1
 end
 puts "do you have a lot on your plate at the moment?".upcase
   value_4 = gets.chomp.to_i
   if value_4 == "yes"
-  
+  stressed += 1
 else value_4 == "no"
-  
+  peaceful += 1
 end
 puts "would music calm you down right now?".upcase
   value_5 = gets.chomp.to_i
 if value_5 == "yes"
-  
+  stressed += 1
 else value_5 == "no"
-  
+  peaceful += 1
 end
 puts "do you have a crush?".upcase
   value_6 = gets.chomp.to_i
 if value_6 == "yes"
-
+  happy += 1
+  stressed += 1
 else value_6 == "no"
-
+  peaceful += 1
 end
 puts "have you been on a vacation recently?".upcase
   value_7 = gets.chomp.to_i
 if value_7 == "yes"
-
+  peaceful += 1
 else value_7 == "no"
-
+  stressed += 1
 end
 puts "do you enjoy watching vines?".upcase
   value_8 = gets.chomp.to_i
 if value_8 == "yes"
-
+  happy += 1
 else value_8 == "no"
-
+  upset += 1
 end
 puts "are you in the mood to laugh with friends?".upcase
   value_9 = gets.chomp.to_i
   if value_9 == "yes"
-
+  happy += 1
 else value_9 == "no"
-
+  upset += 1
 end
 puts "are you living up to your own expectations?".upcase
   value_10 = gets.chomp.to_i
 if value_10 == "yes"
-
+  happy += 1
+  peaceful += 1
 else value_10 == "no"
-
+  stressed += 1 
+  upset += 1
 end
+mood_array = [happy, stressed, upset, peaceful]
+if mood_array.max == happy
+elsif mood_array.max == peaceful
+elsif mood_array.max == upset
+elsif mood_array.max == stressed
 end
 
 puts mood
+puts mood_array
